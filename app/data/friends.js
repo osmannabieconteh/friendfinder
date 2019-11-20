2a1efee9b11c3d9be750e3c4e7c420dd25480458
osman conteh <osmannabieconteh@gmail.com>
2019-11-19 21:33:14 -0500

    resentchanges

--- app/data/friends.js
+++ app/data/friends.js
@@ -5,31 +5,22 @@ var friends = [
     photo: "https://ctl.s6img.com/society6/img/F1hHl-fs3oFVJLKSifMeVj_Wj3s/w_700/prints/~artwork/s6-0024/a/9411591_11139468/~~/boxing-cat-prints.jpg",
     scores: [
       "5",
-      "1",
-      "4",
-      "4",
+     "4",
       "5",
       "1",
       "2",
-      "5",
-      "4",
-      "1"
     ]
+    
   },
   {
     name: "Nabie Junior ",
     photo: "https://thesefootballtimes.co/wp-content/uploads/2019/04/drogba.jpg",
     scores: [
-      "4",
-      "2",
       "5",
-      "1",
-      "3",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2"
+      "4",
+       "5",
+       "1",
+       "2",
     ]
   },
   {
@@ -37,67 +28,47 @@ var friends = [
     photo: "https://storage.googleapis.com/thisday-846548948316-wp-data/wp-media/2018/10/2cf884fd-aliko-dangote-1.jpg",
     scores: [
       "5",
-      "2",
-      "2",
-      "2",
       "4",
-      "1",
-      "3",
-      "2",
-      "5",
-      "5"
+       "5",
+       "1",
+       "2",
     ]
   },
   {
     name: "Osman Freeman",
     photo: "http://images.huffingtonpost.com/2015-02-06-GSEEventImage.jpg",
     scores: [
-      "3",
-      "3",
+      "5",
       "4",
-      "2",
-      "2",
-      "1",
-      "3",
-      "2",
-      "2",
-      "3"
+       "5",
+       "1",
+       "2",
     ]
   },
   {
     name: "Cartter Billy",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
     scores: [
-      "4",
-      "3",
-      "4",
-      "1",
       "5",
-      "2",
-      "5",
-      "3",
-      "1",
-      "4"
+      "4",
+       "5",
+       "1",
+       "2",
     ]
   },
   {
     name: "Steven Nelson",
     photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
     scores: [
+      "5",
       "4",
-      "4",
-      "2",
-      "3",
-      "2",
-      "2",
-      "3",
-      "2",
-      "4",
-      "5"
+       "5",
+       "1",
+       "2",
     ]
   }
 
 ];
 
-// Here we export the array. This makes it accessible to other files using require.
+
 module.exports = friends;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -14,26 +14,36 @@ module.exports = function (app) {
       friendDifference: Infinity
     };
     var userData = req.body;
+
     var userScores = userData.scores;
     var totalDifference;
-
+     
+    console.log(userData);
 
     for (var i = 0; i < friends.length; i++) {
       var currentFriend = friends[i];
       totalDifference = 0;
-      console.log(currentFriend.name);
       for (var j = 0; j < currentFriend.scores.length; j++) {
         var currentFriendScore = currentFriend.scores[j];
         var currentUserScore = userScores[j];
+      
+        console.log(currentFriendScore,"currentFriendScore");
+        console.log(currentUserScore,"currentUserScore");
         totalDifference += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      
       }
+
+      
+
       if (totalDifference <= bestMatch.friendDifference) {
         bestMatch.name = currentFriend.name;
         bestMatch.photo = currentFriend.photo;
         bestMatch.friendDifference = totalDifference;
+
       }
     }
     friends.push(userData);
+    console.log(bestMatch);
     res.json(bestMatch);
   });
 };
